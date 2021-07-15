3d4ec5d140932ac40cb859c4b4b448f43355ce71
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2021-07-15 15:58:03 +0530

    root change

--- textutils/settings.py
+++ textutils/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_URL = '/static/'
+
+STATIC_ROOT = "/var/www/textoperations.herokuapp.com//static/"

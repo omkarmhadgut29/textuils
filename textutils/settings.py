6c71e121c04e05680cc652edd6214786152bd9cf
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2021-07-15 17:11:01 +0530

    updated setting

--- textutils/settings.py
+++ textutils/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'o)9kp!frdn1_!#vz-fs+*+=5v9)yh_z6_6b5k@yghm80$pt__x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'textoperations.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'textoperations.herokuapp.com']
 
 
 # Application definition
